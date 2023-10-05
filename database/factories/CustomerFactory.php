7b118d439f9b915f8557459e464ec299ead4c60b
amazing79 <ignacioja@hotmail.com>
2023-10-05 10:58:37 -0300

    termino parte 2. se creo lo necesario para replicar la base de datos

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Customer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CustomerFactory extends Factory
+{
+    protected $model = Customer::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $type = $this->faker->randomElement(['I', 'B']);
+        $name = $type == 'I' ? $this->faker->name() : $this->faker->company();
+        return [
+            'name' => $name,
+            'type' => $type,
+            'email' => $this->faker->email(),
+            'address' => $this->faker->streetAddress(),
+            'city' => $this->faker->city(),
+            'state' => $this->faker->streetSuffix(),
+            'postal_code' => $this->faker->postcode(),
+        ];
+    }
+}
